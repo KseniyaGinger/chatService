4e979680de37cbd27610cc1463a56f475ae1d51d
Kseniya Ginger <mega_kalina@mail.ru>
2023-08-03 19:22:25 +0300

    Initial commit

---
+++ src/test/kotlin/ChatServiceTest.kt
@@ -0,0 +1,32 @@
+import org.junit.Assert.*
+
+class ChatServiceTest {
+
+    @org.junit.Test
+    fun sendMessage() {
+    }
+
+    @org.junit.Test
+    fun lastMessages() {
+    }
+
+    @org.junit.Test
+    fun getMessage() {
+    }
+
+    @org.junit.Test
+    fun deleteMessage() {
+    }
+
+    @org.junit.Test
+    fun getUnreadChatsCount() {
+    }
+
+    @org.junit.Test
+    fun getChats() {
+    }
+
+    @org.junit.Test
+    fun deleteChat() {
+    }
+}
